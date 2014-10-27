24ecb8b19153996bf874f128e9d41921cd945d2d
hansolo669 <callum1234moo@gmail.com>
2014-10-27 18:18:03 -0400

    finished a2 in c

--- COMP1005/a2/a2.c
+++ COMP1005/a2/a2.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+ * Assignment 2
+ */
 
-int factorial(int n)
-{
+/*
+ * Question 1
+ */
+
+int factorial(int n){
 	int x = 1;
-	for (int y = 1; y < n + 1; ++y)
-	{
-		x = x * y;
+	for (int i = 1; i < n + 1; ++i){
+		x = x * i;
 	}
 	return x;
 }
 
-int add_list(int a[])
-{
+/*
+ * Question 2
+ */
+int add_list(int a[]){
 	int x = 0;
 	int count = sizeof(a[0]);
 	for (int i = 0; i < count; ++i)
@@ -21,20 +30,22 @@ int add_list(int a[])
 	return x;
 }
 
-int average(int a[])
-{
+/*
+ * Question 3
+ */
+int average(int a[]){
 	return add_list(a) / sizeof(a[0]);
 }
 
-int * threshold(int a[], int x)
-{
+/*
+ * Question 4
+ */
+int * threshold(int a[], int x){
 	static int y[] = {0};
 	int yi = 0;
 	int count = sizeof(a[0]);
-	for (int i = 0; i < count; ++i)
-	{
-		if (a[i] >= x)
-		{
+	for (int i = 0; i < count; ++i){
+		if (a[i] >= x){
 			y[yi] = a[i];
 			++yi;
 		}
@@ -42,24 +53,46 @@ int * threshold(int a[], int x)
 	return y;
 }
 
-char bmi_category(int bmi)
-{
-	return 0;
+/*
+ * Question 5
+ */
+char * bmi_category(int bmi){
+    if (bmi < 18.5) {
+        return "underweight";
+    } else if (bmi < 25){
+        return "normal";
+    } else if (bmi < 30){
+        return "overweight";
+    } else {
+        return "obese";
+    }
 }
 
-int llog2(int n)
-{
-	return 0;
+/*
+ * Question 6
+ */
+int llog2(int n){
+    int x = 0;
+    while (n > 1) {
+        n = n / 2;
+        ++x;
+    }
+    return x;
 }
 
-int main(int argc, char const *argv[])
-{
-	printf("%s\n", "hi");
+int main(int argc, char const *argv[]){
+    
 	printf("factorial: %d\n", factorial(5));
 	int aarray[4] = {3, 2, 3, 4};
 	printf("add_list: %d\n", add_list(aarray));
 	printf("average: %d\n", average(aarray));
+    
 	int tarray[5] = {1, 2, 5, 3, 6};
-	printf("%s\n", threshold(tarray, 4));
+	printf("threshold: %d\n", *threshold(tarray, 4));
+    
+    printf("bmi_category: %s\n", bmi_category(18.4));
+    
+    printf("log2: %d\n", llog2(8));
+    
 	return 0;
 }
